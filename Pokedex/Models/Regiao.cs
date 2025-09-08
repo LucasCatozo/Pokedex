c4a008f4e1c04dfb29a6d9fa4029dda440b6ab2d
LucasCatozo <catozolucas@gmail.com>
2025-09-08 20:38:53 -0300

    finalizado aula 1

---
+++ Pokedex/Models/Regiao.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pokedex.Models
+{
+     [Table("Genero")]
+    public class Regiao
+    {
+        [Key]
+        public uint Id { get; set; }
+
+        [StringLength(30)]
+        [Required(ErrorMessage = "Por favor, informe o nome")]
+        public string Nome { get; set; }
+
+
+    }
+}
